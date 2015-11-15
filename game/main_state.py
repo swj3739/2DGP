4f10062acc2f39296402279e2ef8529c81b8b9ee
swj3739 <swj1718@naver.com>
2015-11-15 19:55:52 +0900

    유저 움직임 제한
    유저가 움직일 수 있는 범위를 조정
    좌 우 모두 막힘
    그전에 캐릭터가 깜빡깜빡 거리는 버그를 해결

--- game/main_state.py
+++ game/main_state.py
@@ -4,11 +4,8 @@ import os
 import sys
 sys.path.append('../LabsAll/Labs')
 import game_framework
-
 import main_state2
-
 from pico2d import *
-
 
 
 class Ground:
@@ -18,12 +15,14 @@ class Ground:
     def draw(self):
         self.image.clip_draw(0,0,600,751,400,430)
 
+
 class Audience1:
     def __init__(self):
         self.image = load_image('audience1.png')
 
     def draw(self):
         self.image.clip_draw(0,0,165,751,50,430)
+
 
 class Audience2:
     def __init__(self):
@@ -32,12 +31,14 @@ class Audience2:
     def draw(self):
         self.image.clip_draw(0,0,165,751,750,430)
 
+
 class Ball:
     def __init__(self):
         self.image = load_image('SoccerBall.png')
 
     def draw(self):
         self.image.clip_draw(0,0,50,48,400,200)
+
 
 class User:
     def __init__(self):
@@ -46,14 +47,13 @@ class User:
     def draw(self):
         self.image.clip_draw(0, 0, 100, 112, x, y)
 
+
 class Ai1:
     def __init__(self):
         self.image = load_image('AI1.png')
 
     def draw(self):
         self.image.clip_draw(0,0,100,100,400,750)
-
-
 
 
 def handle_events():
@@ -65,13 +65,33 @@ def handle_events():
             game_framework.quit()
         elif event.type == SDL_MOUSEMOTION:
             if(x<150):
-                x,y=150,800-event.y
+                x,y=149,800-event.y
+                if(event.x>=150):
+
+                    x=150
+
             elif(x>650):
-                x,y=650,800-event.y
+                x,y=651,800-event.y
+                if(event.x<=650):
+
+                    x=650
             elif(x>=150 and x<=650):
                 x,y = event.x,800-event.y
-            elif(y>430):
-                x,y=event.x,430
+
+
+            if(y>390):
+                x,y=event.x,390
+                if(x<150):
+                    x=149
+                elif(x>650):
+                    x=650
+            elif(y<100):
+                 x,y=event.x,100
+                 if(x<150):
+                    x=149
+                 elif(x>650):
+                    x=650
+
 
         elif event.type == SDL_KEYDOWN and event.key ==SDLK_ESCAPE:
             game_framework.quit()
@@ -89,6 +109,7 @@ running=True
 x, y = 100, 100
 hide_cursor()
 
+
 def enter():
     global ground,audience1,audience2,user,ball,ai1
     open_canvas(800,800)
@@ -98,6 +119,7 @@ def enter():
     audience2=Audience2()
     ball=Ball()
     ai1=Ai1()
+
 
 def exit():
     global ground,audience1,audience2,user,ball,ai1
@@ -109,8 +131,10 @@ def exit():
     del(ai1)
     close_canvas()
 
+
 def update():
     pass
+
 
 def draw():
     clear_canvas()
@@ -121,6 +145,7 @@ def draw():
     ball.draw()
     ai1.draw()
     update_canvas()
+
 
 def pause():
     pass
